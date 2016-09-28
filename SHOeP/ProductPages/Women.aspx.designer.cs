12a0022aef2f2771466ae96250afc01df4f8d774
Kriszta <Kriszta@DESKTOP-QC3MALE>
2016-09-28 13:20:24 +0200

    EN DropDowns fix mot sök

--- SHOeP/ProductPages/Women.aspx.designer.cs
+++ SHOeP/ProductPages/Women.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace SHOeP.ProductPages {
     public partial class Women {
         
         /// <summary>
+        /// DropDownList1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+        
+        /// <summary>
         /// modelList control.
         /// </summary>
         /// <remarks>
