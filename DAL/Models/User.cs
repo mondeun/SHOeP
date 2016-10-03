306fc0dfda47a75be312485e44897ca0745ed928
Kenneth Glawe <kenneth.glawe@gmail.com>
2016-10-03 02:50:59 +0200

    Salt fix

--- DAL/Models/User.cs
+++ DAL/Models/User.cs
@@ -39,7 +39,7 @@ namespace DAL.Models
         private void CreateSalt()
         {
             var rng = RandomNumberGenerator.Create();
-            var bytes = new byte[15];
+            var bytes = new byte[16];
             rng.GetBytes(bytes);
             Salt = Convert.ToBase64String(bytes);
         }
