5dc2696ea8a3c4194fb477540aca86476530372b
Kenneth Glawe <kenneth.glawe@gmail.com>
2016-10-04 13:27:29 +0200

    Minor change in Register

--- SHOeP/Account/Register.aspx.cs
+++ SHOeP/Account/Register.aspx.cs
@@ -20,14 +20,16 @@ namespace SHOeP.Account
         {
             if (passBox.Text != confirmPassBox.Text) return;
 
-            User newUser = new DAL.Models.User();
-            newUser.FirstName = forenameBox.Text;
-            newUser.LastName = lastnameBox.Text;
-            newUser.Email = emailBox.Text;
-            newUser.Address = addressBox.Text;
-            newUser.City = cityBox.Text;
-            newUser.Zip = zipBox.Text;
-            newUser.Password = passBox.Text;
+            var newUser = new DAL.Models.User
+            {
+                FirstName = forenameBox.Text,
+                LastName = lastnameBox.Text,
+                Email = emailBox.Text,
+                Address = addressBox.Text,
+                City = cityBox.Text,
+                Zip = zipBox.Text,
+                Password = passBox.Text
+            };
             newUser.HashAndSaltPassword();
 
             UserController usercon = new UserController();
