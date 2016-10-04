97d2a97dcd875432ecbde7f6ec2d63f96d429bb1
Kenneth Glawe <kenneth.glawe@gmail.com>
2016-10-04 12:05:24 +0200

    Fixed User.FromSqlreader

--- DAL/Models/User.cs
+++ DAL/Models/User.cs
@@ -34,6 +34,7 @@ namespace DAL.Models
             Zip = reader["Zip"].ToString();
             City = reader["City"].ToString();
             Password = reader["Password"].ToString();
+            Salt = reader["salt"].ToString();
         }
 
         private void CreateSalt()
